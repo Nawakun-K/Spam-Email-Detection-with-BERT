26c3faf38454af20f3e537f53d8ec0cac28e305c
Nawakun-K <nawakun.k@gmail.com>
2021-09-19 15:35:04 +0700

    Modify /app/main.py, add model sections

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from loguru import logger
 
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix , classification_report
+
+from services.spam_detect import SpamEmailDetect
 
 @logger.catch
 def main(
@@ -19,8 +24,40 @@ def main(
 
     # Preprocessing data
     df_balanced['spam'] = df_balanced['Category'].apply(lambda x:1 if x == 'spam' else 0)
-    print(df_balanced.sample(4))
 
+    # Loading train-test-split
+    X_train, X_test , y_train, y_test = train_test_split(df_balanced['Message'], df_balanced['spam'],
+                                                    stratify = df_balanced['spam'])
+    
+    # Instantiate model
+    sed = SpamEmailDetect()
+    logger.debug("----------Model Summary----------")
+    sed.summary()
+
+    # Compile model
+    sed.compile()
+
+    # Fit data into model
+    logger.debug("----------Model Fitting----------")
+    sed.fit(X_train, y_train, epochs=1)
+
+    # Make prediction and evaluate
+    logger.debug("----------Model Evaluation----------")
+    sed.evaluate(X_test, y_test)
+
+    y_pred = sed.predict(X_test)
+    y_pred = np.round(y_pred.flatten())
+
+    # Confusion matrix
+    logger.debug("----------Confusion Matrix----------")
+    cm = confusion_matrix(y_test, y_pred)
+    df = pd.DataFrame(cm, index=['Actual Negative', 'Actual Positive'], columns=['Predicted Negative', 'Predicted Positive'])
+    print(df)
+
+    # Classification report
+    logger.debug("----------Classification Report----------")
+    cr = classification_report(y_test, y_pred)
+    print(cr)
 
 if __name__ == "__main__":
     main()
